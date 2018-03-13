0dfbfd4d7be7308fa14d2d2a2c0ee2cdf1878da7
Akkapong Kajornwongwattana <akk.ohm@gmail.com>
2018-03-13 14:39:11 +0700

    update config

--- src/config/help.php
+++ src/config/help.php
@@ -4,7 +4,14 @@ return [
         'method_list' => ['getRoleList', 'getRoleDetail', 'createRole', 'updateRole', 'deleteRole'],
         //Role
         'getRoleList' => [
-            'params' => [],
+            'params' => [
+                'name'     => 'not required',
+                'ref_type' => 'not required',
+                'ref_id'   => 'not required',
+                'limit'    => 'not required',
+                'offset'   => 'not required',
+                'order_by' => 'not required (ex => name:asc)',
+            ],
             'output' => [
                 [
                     'attributes.name'       => 'Role name',
@@ -18,7 +25,7 @@ return [
         ],
         'getRoleDetail' => [
             'params' => [
-                'id' => 'Role ID',
+                'id' => 'required',
             ],
             'output' => [
                 'attributes.name'        => 'Role name',
@@ -76,7 +83,17 @@ return [
         ],
         //Permission
         'getPermissionList' => [
-            'params' => [],
+            'params' => [
+                'module'     => 'not required',
+                'role_id'    => 'not required',
+                'can_read'   => 'not required',
+                'can_create' => 'not required',
+                'can_update' => 'not required',
+                'can_delete' => 'not required',
+                'limit'      => 'not required',
+                'offset'     => 'not required',
+                'order_by'   => 'not required (ex => module:asc)',
+            ],
             'output' => [
                 [
                     'attributes.module'     => 'Module name',
@@ -165,5 +182,23 @@ return [
                 'attributes.updated_at' => 'Update date time',
                 'id'                    => 'Permission ID',
             ]
+        ],
+        'configs' => [
+            'role' => [
+                'url'    => 'http://ms-role-api.develop/',
+                'list'   => 'roles',
+                'detail' => 'roles/[id]',
+                'create' => 'roles',
+                'update' => 'roles/[id]',
+                'delete' => 'roles/[id]',
+            ],
+            'permission' => [
+                'url'    => 'http://ms-role-api.develop/',
+                'list'   => 'permissions',
+                'detail' => 'permissions/[id]',
+                'create' => 'permissions',
+                'update' => 'permissions/[id]',
+                'delete' => 'permissions/[id]',
+            ],
         ]
     ];
